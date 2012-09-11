b6eba40b3e10a850f15be5e5030d3c59aceec9e8
Daniel Norris <dnorris10@gmail.com>
2012-09-10 21:56:16 -0700

    Change API handling
    - Fogbugs class becomes API
    - Calls to api are handled through eval_call

--- fogbugz-timeline.rb
+++ fogbugz-timeline.rb
@@ -1,5 +1,27 @@
 require 'bundler'
+require 'optparse'
 require './lib/fogbugz'
 
-fog = FogBugz::FogBugz.new File.absolute_path('config.yaml')
-fog.make_request(:listIntervals)
+options = {}
+
+opts = OptionParser.new do |op|
+  op.banner = "usage: fogbugz-timeline <start|stop> case"
+
+  op.on('-v', '--[no]-verbose', 'Run verbosely') do |v|
+    options[:verbose] = v
+  end
+
+end
+
+#if ARGV.size < 2
+#  puts opts.banner
+#  exit
+#end
+
+opts.parse!
+
+cmd = ARGV[0].to_s
+case_num = ARGV[1]
+
+fog = FogBugz::API.new File.absolute_path('config.yaml')
+fog.eval_call(cmd,case_num)
--- lib/fogbugz.rb
+++ lib/fogbugz.rb
@@ -3,13 +3,32 @@ require 'nokogiri'
 require 'httparty'
 
 module FogBugz
-  class FogBugz
+  class API
     include HTTParty
+
+    class APIError < StandardError
+    end
 
     def initialize(config_file)
       @config = Psych.load(File.open(config_file,'r'))
       @token = @config[:token]
       @base_url = @config[:base_url]
+    end
+
+    def eval_call(cmd, *options)
+      if self.method(cmd).arity > 0
+        if self.method(cmd).arity == options.size
+          self.send(cmd, *options)
+        else
+          raise APIError
+        end
+      else
+        self.send(cmd)
+      end
+    end
+
+    def list_intervals
+      make_request(:listIntervals)
     end
 
     def make_request(command, params={})
@@ -19,8 +38,7 @@ module FogBugz
       request.merge!({:cmd => command})
       request.merge! params
       request = {:body => request}
-      doc = Nokogiri::XML(self.class.post(@base_url, request).body)
-      puts doc
+      Nokogiri::XML(self.class.post(@base_url, request).body)
     end
   end
 
